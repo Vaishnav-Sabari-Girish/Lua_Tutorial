ec4ef1d387ce14db94601809a1af7b8714c0d95d
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-05-30 17:30:57 +0530

    Added variables program

---
+++ variables/variables.lua
@@ -0,0 +1,17 @@
+-- This file tells us about variables
+
+num = 42
+
+ss = "String with single quotes"
+sd = "String with double quotes"
+
+-- Both ss and sd are the same
+
+sm = [[Multi-line
+Strings
+]]
+
+print("Integer: ", num)
+print("Single quote string: ", ss)
+print("Double quote string: ", sd)
+print("Multi-Line string: ", sm)
